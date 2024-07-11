d80118f08b68a5ed37f92abd3eae1e3b11bdd44f
Nandini U <154657647+Nandiniuppar@users.noreply.github.com>
2024-07-11 23:14:25 +0530

    updates

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <div className= "leftnav">
+        <a href="/" className="logo">logo</a>
+       </div>
+       <div className="right-nav">
+        <a class="nav-link" href="/">Home</a>
+        <a class="nav-link" href="/">Thematic Area</a>
+        <a class="nav-link" href="/">support</a>
+        <a class="nav-link" href="/">contact</a>
+        <a class="nav-link" href="/">contact</a>
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
